a119910873bd6c4e16c875fb8a8d13105e523187
James Brooking <jambroo@gmail.com>
2017-10-04 12:17:07 +0100

    Adding constants for dublin office, max distance and the earth's radius.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict'
 
+const DUBLIN_OFFICE = {lat: 53.3393, long: -6.2576841};
+const MAX_DISTANCE = 100; // KM
+const EARTH_RADIUS = 6371; // KM - from https://en.wikipedia.org/wiki/Earth_radius
+
 /*
   This will contain the main funcion to read the JSON and output customers within 100km of the Dublin office
 */
